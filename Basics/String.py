50e7023d2e1dcff7404700987c40fee19899328c
dhruvpahwa29 <dhruvpahwa29@gmail.com>
2020-08-24 20:59:38 +0530

    STRING DECLARATION

---
+++ Basics/String.py
@@ -0,0 +1,8 @@
+a ='HELLO DHRUV'
+print(a)
+a = "HELLO DHRUV"
+print(a)
+#The difference between the two is that using double quotes makes it easy to include apostrophes
+#(whereas these would terminate the string if using single quotes)
+b = "Don't worry about apostrophes"
+print(b)
