e6ce1f261aee124b2444fc46653b2f59628d0830
dhruvpahwa29 <dhruvpahwa29@gmail.com>
2020-08-29 23:53:32 +0530

    PRINT EMOJIS USING CODES

---
+++ Basics/PrintEmojis.py
@@ -0,0 +1,4 @@
+print("\U0001F60D")
+#this is the code for emoji which is taken from unicode.org and '+' in code is replaced by'000' and the code for emoji is always followed by a backslash
+print("\U0001F604")
+print("\U0001F618")
