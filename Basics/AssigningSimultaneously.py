a6ab37de4440ee712e34826d248ebf6b6a550b56
dhruvpahwa29 <69198928+dhruvpahwa29@users.noreply.github.com>
2020-09-13 17:35:32 +0530

    ASSIGNING VALUES TO MORE THAN ONE VARIABLE SIM.

--- Basics/AssigningSimultaneously.py
+++ Basics/AssigningSimultaneously.py
@@ -3,3 +3,9 @@ a,b = 6,4
 print(a,b)
 c,d,e,f = "d","v","c","p"
 print(c,d,e,f)
+
+name,age = "DHRUV","20"
+print("HELLO "+name+" YOUR AGE IS "+age)
+x=y=z=1
+#DECLARING MORE THAN ONE VALUES IN A SINGLE LINE 
+print(x+y+z)
